e7ec2fba37345a76e4aa826d1461038ace83f13b
Rens Oliemans <rens.oliemans@gmail.com>
2018-12-17 14:52:59 +0100

    :art: Made code neater

--- contents/js/nrc.js
+++ contents/js/nrc.js
@@ -1,3 +1,13 @@
+function buildText(name, value, path, domain) {
+  return `${name}=${value};path=${path};domain=${domain}`;
+}
+
+
 var name = 'counter';
-console.log(`Setting cookie with name ${name} to 0.`)
-document.cookie = name + '=0;path=/';
+var value = '0';
+var path = '/';
+var text = buildText(name, value, path, "");
+
+document.cookie = text;
+
+console.log(`Set cookie to ${text}`)
--- contents/js/volkskrant.js
+++ contents/js/volkskrant.js
@@ -1,3 +1,13 @@
+function buildText(name, value, path, domain) {
+  return `${name}=${value};path=${path};domain=${domain}`;
+}
+
 var name = 'TID_ID';
-console.log(`Setting cookie with name ${name} to 0.`)
-document.cookie = name + '=;path=/;domain=.volkskrant.nl';
+var value = '';
+var path = '/';
+var domain = '.volkskrant.nl';
+var text = buildText(name, value, path, domain)
+
+document.cookie = text;
+
+console.log(`Set cookie to ${text}`)
